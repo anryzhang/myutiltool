b19d6d60d940b30b67167bfdae76aba8278e97a7
ruizhang <zhang.rui@yuejian.com>
2017-08-25 15:04:14 +0800

    update

--- index.js
+++ index.js
@@ -10,3 +10,19 @@ exports.sum = function () {
     }
     return _sum;
 }
+
+exports.encrypt = function (val) {
+    let arr_val = val.split('');
+    let asc = arr_val.map((cur)=>{
+        return (cur.charCodeAt(0)+10)*2;
+    });
+    return encodeURIComponent(asc.join());
+}
+
+exports.unEncrypt = function (val) {
+    let arr_val = decodeURIComponent(val).split(',');
+    let _str = arr_val.map((cur)=>{
+        return String.fromCharCode(cur/2-10);
+    });
+    return _str.join('');
+}
